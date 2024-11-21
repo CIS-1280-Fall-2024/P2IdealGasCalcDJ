61ceb407e4e621e705422fd9f6bd026a01730afd
jaram <jaram@MSJARAMILLO>
2024-11-20 18:09:39 -0700

    Add project files.

---
+++ MauiApp1/IdealGas.cs
@@ -0,0 +1,93 @@
+﻿// Your Name // Your Email // IdealGas.cs // Date
+
+public class IdealGas
+{
+    private double mass;
+    private double volume;
+    private double temp;
+    private double molecularWeight;
+    private double pressure;
+
+    public object? PV { get; private set; }
+
+    private double P;
+
+    public object? nRT { get; private set; }
+    public object? constant { get; private set; }
+    public int mol { get; private set; }
+    public object? K { get; private set; }
+    public int J { get; private set; }
+    public double R { get; private set; }
+
+    public void SetMass(double mass)
+    { this.mass = mass;
+        Calc(GetConstant(), R, GetK());
+    }
+    public double GetMass()
+    {
+        return mass;
+    }
+    public void SetVolume(double volume)
+    { this.volume = volume;
+        Calc(GetConstant(), R, GetK());
+    }
+    public double GetVolume()
+    { return volume;
+    } public void SetTemp(double temp)
+    { this.temp = temp; Calc(GetConstant(), R, GetK());
+    } public double GetTemp()
+    { return temp;
+    }
+    public void SetMolecularWeight(double molecularWeight)
+    {
+        this.molecularWeight = molecularWeight; Calc(GetConstant(), R, GetK());
+    }
+    public double GetMolecularWeight()
+    {
+        return molecularWeight;
+    }
+    public double GetPressure()
+    {
+        return pressure;
+    }
+
+    private object GetConstant()
+    {
+        return constant;
+    }
+
+    private object GetK()
+    {
+        return K;
+    }
+
+    private void Calc(object constant, double r, object k)
+    {
+        PV = nRT;
+        P = (mass / molecularWeight) * r * temp / volume; const double R = 8.314;
+        int mol2 = (mol);
+        Kdouble v = mass / molecularWeight * R * temp;
+        pressure = v;
+            }
+}
+
+internal class Kdouble
+{
+    public static implicit operator Kdouble(double v)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static implicit operator double(Kdouble v)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal class GetConstant
+{
+}
+
+internal class Universal
+{
+}
---
+++ MauiApp1/program.cs
@@ -0,0 +1,37 @@
+﻿public class Program
+    {
+        public static void DisplayHeader() 
+    { Console.WriteLine("Your Name - Ideal Gas Equation Program"); 
+        Console.WriteLine("This program calculates the pressure of an ideal gas.");
+    }
+        public static void GetMolecularWeights(out string[] gasNames, out double[] molecularWeights) 
+    { 
+        gasNames = new string[] 
+        { "Oxygen", "Nitrogen", "Carbon Dioxide"
+        }; molecularWeights = new double[] 
+        { 32.00, 28.02, 44.01 
+        }; 
+    }
+        public static void DisplayGasNames(string[] gasNames) 
+    { Console.WriteLine("Available gases:"); 
+        for (int i = 0; i < gasNames.Length; i++) 
+        { 
+            Console.WriteLine($"{i + 1}. {gasNames[i]}"); 
+        }
+    }
+        public static double GetMolecularWeightFromName(string name, string[] gasNames, double[] molecularWeights) 
+    { 
+        for (int i = 0; i < gasNames.Length; i++) 
+        { 
+            if (gasNames[i].Equals(name, StringComparison.OrdinalIgnoreCase)) 
+            { return molecularWeights[i]; 
+            } 
+        } throw new Exception("Gas not found."); 
+    }
+        public static void DisplayPressure(double pressure) 
+    { 
+        Console.WriteLine($"Pressure: {pressure} Pa");
+    }
+        public static double PaToPSI(double pressure) { return pressure * 0.000145038; 
+    }
+}
